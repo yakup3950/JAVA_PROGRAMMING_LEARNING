12f69ecbcb0aa0b68f0f250bc648ccc477b608f4
Pascal Seasharp <yakup.coskun42@gmail.com>
2024-03-11 13:00:50 +0100

    Added exercises

---
+++ src/m11_operators_part1/ImplicitCasting.java
@@ -0,0 +1,16 @@
+package m11_operators_part1;
+
+public class ImplicitCasting {
+    public static void main(String[] args) {
+
+        /*int num1 = 25;
+        byte num2 ;
+
+        num2 = (byte)num1; //Explicit casting*/
+
+        byte num3 = 100;
+        int num4 ;
+        num4 = num3; //Implicit casting
+
+    }
+}
