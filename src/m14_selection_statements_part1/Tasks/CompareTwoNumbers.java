3e6c016044b988554d53a10ee84ae68f492bbfd5
Pascal Seasharp <yakup.coskun42@gmail.com>
2024-03-14 06:23:42 +0100

    Coming soon

--- src/m14_selection_statements_part1/Tasks/CompareTwoNumbers.java
+++ src/m14_selection_statements_part1/Tasks/CompareTwoNumbers.java
@@ -3,10 +3,20 @@ package m14_selection_statements_part1.Tasks;
 public class CompareTwoNumbers {
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 20;
+        int a = -30;
+        int b = 30;
+        String result;
 
-
+    if(a != b){
+        if(a > b){
+            result = a + " greater than " + b;
+        }else {
+             result = b + " greater than " + a;
+        }
+        System.out.println(result);
+    }else {
+        System.err.println("NUMBERS ARE EQUAL");
+    }
 
     }
 }
